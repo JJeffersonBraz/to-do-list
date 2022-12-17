f4b1ef5a758464ebfdf23eca1a9143cdc9ad7e5b
Jefferson Braz <jjeffersonbraz@gmail.com>
2022-12-16 21:34:48 -0300

    style: Melhora no estilo das tarefas inseridas

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ function add() {
     if (tarefa.value.length == 0) {
         alert('Por favor, insira uma tarefa.') 
     } else {
-        document.querySelector('div#tarefas').innerHTML += `${tarefa.value}`
+        document.querySelector('div#tarefas').innerHTML += `
+            <div class="tarefas">
+                <span>
+                    ${tarefa.value} 
+                </span>
+                <button class="excluir"> X </button>
+            </div>
+        `
         tarefa.value = ""
         tarefa.focus
     }
